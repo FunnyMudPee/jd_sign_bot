aef1ec7732dcda296f069226a30ab58cba562ccc
FunnyMudPee <30362361+FunnyMudPee@users.noreply.github.com>
2021-03-07 20:31:07 +0800

    添加打印信息

--- app.js
+++ app.js
@@ -56,21 +56,26 @@ async function start() {
   // 执行
   await exec("node JD_DailyBonus.js >> result.txt");
   console.log('执行完毕')
-
-  if (serverJ) {
     const path = "./result.txt";
     let content = "";
     if (fs.existsSync(path)) {
       content = fs.readFileSync(path, "utf8");
     }
-    let t = content.match(/【签到概览】:((.|\n)*)【签到总计】/)
-    let res = t ? t[1].replace(/\n/,'') : '失败'
-    let t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/)
-    let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
+    console.log(content);
+//   if (serverJ) {
+//     const path = "./result.txt";
+//     let content = "";
+//     if (fs.existsSync(path)) {
+//       content = fs.readFileSync(path, "utf8");
+//     }
+//     let t = content.match(/【签到概览】:((.|\n)*)【签到总计】/)
+//     let res = t ? t[1].replace(/\n/,'') : '失败'
+//     let t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/)
+//     let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
 
     
-    await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
-  }
+//     await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
+//   }
 }
 
 start()
